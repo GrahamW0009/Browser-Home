351b569684aa2b4f0f82fe7008a0913ddfb65b9a
Graham Walters <git@grahamwalters.me>
2013-03-08 15:04:12 +0000

    Renamed currentX to x. Added Month, Week, Day to update()
    currentTime -> now
    currentHours -> hours
    currentMinutes -> minutes
    ...

--- js/main.js
+++ js/main.js
@@ -1,14 +1,33 @@
 function update() {
-  var currentTime = new Date();
-  var currentHours = currentTime.getHours();
-  var currentMinutes = currentTime.getMinutes();
-  var currentSeconds = currentTime.getSeconds();
-  /* Convert the hours component to 12-hour format if needed */
-  currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
-  /* Pad the minutes and seconds with leading zeros, if required */
-  currentHours = ( currentHours < 10 ? "0" : "" ) + currentHours;
-  currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
-  currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
+  var now = new Date();
 
-  $('#time').html(currentHours + ":" + currentMinutes);
+  var month = now.getMonth();
+  var week = now.getDay();
+  var day = now.getDate();
+
+  var hours = now.getHours();
+  var minutes = now.getMinutes();
+  var seconds = now.getSeconds();
+
+  /* Convert the hours component to 12-hour format if needed */
+  hours = ( hours > 12 ) ? hours - 12 : hours;
+  /* Pad the minutes and seconds with leading zeros, if required */
+  hours = ( hours < 10 ? "0" : "" ) + hours;
+  minutes = ( minutes < 10 ? "0" : "" ) + minutes;
+  seconds = ( seconds < 10 ? "0" : "" ) + seconds;
+
+  var months = new Array("JANUARY","FEBRUARY","MARCH","APRIL","MAY",
+    "JUNE","JULY","AUGUST","SEPTEMBER","OCTOBER","NOVEMBER","DECEMBER");
+
+  var weeks = new Array("SUNDAY","MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY","SATURDAY");
+
+  var days = new Array ("0", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE", "TEN",
+    "ELEVEN", "TWELVE", "THIRTEEN", "FOURTEEN", "FIFTEEN", "SIXTEEN", "SEVENTEEN", "EIGHTEEN", "NINETEEN", "TWENTY", 
+    "TWENTY-ONE", "TWENTY-TWO", "TWENTY-THREE", "TWENTY-FOUR", "TWENTY-FIVE", "TWENTY-SIX",
+    "TWENTY-SEVEN", "TWENTY-EIGHT", "TWENTY-NINE" ,"THIRTY", "THIRTY-ONE");
+
+  $('#time').html(hours + ":" + minutes);
+  $('#day').html(days[day]);
+  $('#week').html(weeks[week]);
+  $('#month').html(months[month]);
 }
