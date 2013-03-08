22d8fc8010fef5eb02df20b4df540d770becd2bc
Graham Walters <git@grahamwalters.me>
2013-03-08 14:43:32 +0000

    Added Time and Date divides. Added update function to main.js
    update() currently updates the time. There is code for seconds, but I
    don't think I'll end up using it.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+function update() {
+  var currentTime = new Date();
+  var currentHours = currentTime.getHours();
+  var currentMinutes = currentTime.getMinutes();
+  var currentSeconds = currentTime.getSeconds();
+  /* Convert the hours component to 12-hour format if needed */
+  currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
+  /* Pad the minutes and seconds with leading zeros, if required */
+  currentHours = ( currentHours < 10 ? "0" : "" ) + currentHours;
+  currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
+  currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
 
+  $('#time').html(currentHours + ":" + currentMinutes);
+}
