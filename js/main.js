210ace11b433bdd3b1417c727b034a2bf3ce830e
Graham Walters <git@grahamwalters.me>
2016-10-14 23:47:29 +0100

    Cleaned up the code. Removed the jQuery dependancy

--- js/main.js
+++ js/main.js
@@ -1,40 +1,77 @@
-function update() {
-  var now = new Date();
+;(function(window, document, undefined) {
+    'use strict';
 
-  var month = now.getMonth();
-  var week = now.getDay();
-  var day = now.getDate();
+    var MONTHS = ['JANUARY', 'FEBRUARY', 'MARCH', 'APRIL', 'MAY', 'JUNE', 'JULY',
+                  'AUGUST', 'SEPTEMBER', 'OCTOBER', 'NOVEMBER', 'DECEMBER'];
 
-  var hours = now.getHours();
-  var minutes = now.getMinutes();
-  var seconds = now.getSeconds();
+    var WEEKDAYS = ['SUNDAY', 'MONDAY', 'TUESDAY', 'WEDNESDAY',
+                    'THURSDAY', 'FRIDAY', 'SATURDAY'];
 
-  /* Convert the hours component to 12-hour format if needed */
-  hours = ( hours > 12 ) ? hours - 12 : hours;
-  /* Pad the minutes and seconds with leading zeros, if required */
-  hours = ( hours < 10 ? "0" : "" ) + hours;
-  minutes = ( minutes < 10 ? "0" : "" ) + minutes;
-  seconds = ( seconds < 10 ? "0" : "" ) + seconds;
+    var DAYS = ['ONE', 'TWO', 'THREE', 'FOUR', 'FIVE', 'SIX', 'SEVEN',
+              'EIGHT', 'NINE', 'TEN', 'ELEVEN', 'TWELVE', 'THIRTEEN',
+              'FOURTEEN', 'FIFTEEN', 'SIXTEEN', 'SEVENTEEN', 'EIGHTEEN',
+              'NINETEEN', 'TWENTY', 'TWENTY-ONE', 'TWENTY-TWO', 'TWENTY-THREE',
+              'TWENTY-FOUR', 'TWENTY-FIVE', 'TWENTY-SIX', 'TWENTY-SEVEN',
+              'TWENTY-EIGHT', 'TWENTY-NINE' ,'THIRTY', 'THIRTY-ONE'];
 
-  var months = new Array("JANUARY","FEBRUARY","MARCH","APRIL","MAY",
-    "JUNE","JULY","AUGUST","SEPTEMBER","OCTOBER","NOVEMBER","DECEMBER");
 
-  var weeks = new Array("SUNDAY","MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY","SATURDAY");
+    var elements = {
+        time: document.getElementById('time'),
+        day: document.getElementById('day'),
+        weekday: document.getElementById('weekday'),
+        month: document.getElementById('month'),
+    };
 
-  var days = new Array ("0", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE", "TEN",
-    "ELEVEN", "TWELVE", "THIRTEEN", "FOURTEEN", "FIFTEEN", "SIXTEEN", "SEVENTEEN", "EIGHTEEN", "NINETEEN", "TWENTY", 
-    "TWENTY-ONE", "TWENTY-TWO", "TWENTY-THREE", "TWENTY-FOUR", "TWENTY-FIVE", "TWENTY-SIX",
-    "TWENTY-SEVEN", "TWENTY-EIGHT", "TWENTY-NINE" ,"THIRTY", "THIRTY-ONE");
 
-  $('#time').html(hours + ":" + minutes);
-  $('#day').html(days[day]);
-  $('#week').html(weeks[week]);
-  $('#month').html(months[month]);
-}
+    function update() {
+        var now = new Date();
 
-function getURLParameter(name) {
-  return (RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1];
-}
+        var month = now.getMonth();
+        var weekday = now.getDay();
+        var day = now.getDate() - 1;
 
-if (getURLParameter('background') != null)
-  $('html').css('background-image', 'url('+getURLParameter('background')+')');
+        var hours = now.getHours();
+        var minutes = now.getMinutes();
+        var seconds = now.getSeconds();
+
+
+        // Convert the hours component to 12-hour format if needed
+        hours = ( hours > 12 ) ? hours - 12 : hours;
+
+        // Pad the hours, minutes & seconds with leading zeros, if required
+        hours = ( hours < 10 ? '0' : '' ) + hours;
+        minutes = ( minutes < 10 ? '0' : '' ) + minutes;
+        seconds = ( seconds < 10 ? '0' : '' ) + seconds;
+
+
+        // Update elements
+        elements.time.innerHTML = hours +':'+ minutes;
+        elements.day.innerHTML = DAYS[day];
+        elements.weekday.innerHTML = WEEKDAYS[weekday];
+        elements.month.innerHTML = MONTHS[month];
+    }
+
+    setInterval(update, 1000);
+
+
+    //
+    // Custom background image JS
+    //
+
+    function getURLParameter(name) {
+        return (RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[null,null])[1];
+    }
+
+    if (getURLParameter('background') !== null) {
+        var imgURL = getURLParameter('background');
+        document.body.style['background-image'] = 'url('+imgURL+')';
+    }
+
+})(this, this.document);
+
+
+
+
+
+
+
